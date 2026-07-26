69ddf5b35aa42815f505e326e0c7c924cae11fca
Test <in.haseeb@msn.com>
2026-07-27 03:52:42 +0530

    Bump to v1.0.2

--- macwash.rb
+++ macwash.rb
@@ -1,10 +1,10 @@
 class Macwash < Formula
   desc "Clean, optimize and speed up your Mac. Free and open source macOS cleaner CLI"
   homepage "https://github.com/toolka/MacWash"
-  url "https://github.com/toolka/MacWash/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "32ef5e28e8895bef59959bd2a39ab7df2d53be05e2551e2f4ca082297e3f32a6"
+  url "https://github.com/toolka/MacWash/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "71c8387d325e99aa70f2d8185d28e856e27522aaf62023943485fbcad1cbb2e7"
   license "MIT"
-  version "1.0.1"
+  version "1.0.2"
 
   def install
     bin.install "macwash"
