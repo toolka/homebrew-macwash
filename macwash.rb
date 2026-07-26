a37499fc8fb29c5be6e3db23ec4570b208c27888
Test <in.haseeb@msn.com>
2026-07-27 02:54:09 +0530

    Bump to v1.0.1 — fix uninstall menu

--- macwash.rb
+++ macwash.rb
@@ -1,10 +1,10 @@
 class Macwash < Formula
   desc "Clean, optimize and speed up your Mac. Free and open source macOS cleaner CLI"
   homepage "https://github.com/toolka/MacWash"
-  url "https://github.com/toolka/MacWash/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "8e099eb776c53f6005a669f52ceee0d512a8628a283003dcda6b5263a288a1f1"
+  url "https://github.com/toolka/MacWash/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "32ef5e28e8895bef59959bd2a39ab7df2d53be05e2551e2f4ca082297e3f32a6"
   license "MIT"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "macwash"
