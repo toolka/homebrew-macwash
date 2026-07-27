c95459bd15207c56f50cd41a01ec1233f266d226
Test <in.haseeb@msn.com>
2026-07-27 18:24:43 +0530

    Update to v1.0.6 - Matrix green highlight

--- macwash.rb
+++ macwash.rb
@@ -1,10 +1,10 @@
 class Macwash < Formula
   desc "Clean, optimize and speed up your Mac. Free and open source macOS cleaner CLI"
   homepage "https://github.com/toolka/MacWash"
-  url "https://github.com/toolka/MacWash/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "5c1b3c3e2578fe7a53c6b2af8cd15221dceb2d5c21805cae483dba6bfd375615"
+  url "https://github.com/toolka/MacWash/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "97471b8979d92b758e08d259f8bbcc33ceb5eff68b0923ece2d14784e8198439"
   license "MIT"
-  version "1.0.5"
+  version "1.0.6"
 
   def install
     bin.install "macwash"
